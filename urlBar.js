d773545ba52e66db1580eb3274407fa123ddf36b
Rickard Doverfelt <rickard@SolArch-2.0>
2015-07-17 19:17:32 +0200

    Initial commit

---
+++ urlBar.js
@@ -0,0 +1,26 @@
+var ipc = require('ipc');
+
+var bar = document.getElementsByName('urlBar')[0];
+var button = document.getElementsByName('button');
+var webview = document.getElementById("pageView");
+
+function goToPage() {
+  var url;
+  if (bar.value.indexOf("http://") < 0 || bar.value.indexOf("https://")) {
+    url = "http://" + bar.value;
+  } else {
+    url = bar.value;
+  }
+
+  webview.setAttribute("src", url);
+}
+
+bar.addEventListener("keypress", function(e) {
+  if (e.keyCode == 13) {
+    goToPage();
+  }
+}, false);
+
+webview.addEventListener('did-stop-loading', function(status) {
+  bar.value = webview.getUrl();
+});
