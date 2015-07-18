4382d17e93480e7ecb6c5a56169ccdec99302b9f
Rickard Doverfelt <rickard@doverfelt.se>
2015-07-18 22:56:25 +0200

    Made pref show current settings

--- pref.js
+++ pref.js
@@ -1,5 +1,7 @@
 var config = window.parent.require('./configUtil.js');
 
+//Display current config in prefs
+
 function setTheme(theme) {
   if (theme === 'dark') {
     document.getElementsByName('dark')[0].classList.add('selectedBtn');
@@ -13,4 +15,16 @@ function setTheme(theme) {
 
 function setHomepage() {
   config.setProperty('home', document.getElementsByName('homepage')[0].value);
+}
+
+function onload() {
+  var curTheme = config.getProperty('theme');
+  if (curTheme === 'dark') {
+    document.getElementsByName('dark')[0].classList.add('selectedBtn');
+    document.getElementsByName('light')[0].classList.remove('selectedBtn');
+  } else if (curTheme === 'light') {
+    document.getElementsByName('light')[0].classList.add('selectedBtn');
+    document.getElementsByName('dark')[0].classList.remove('selectedBtn');
+  }
+  document.getElementsByName('homepage')[0].value = config.getProperty('home');
 }
