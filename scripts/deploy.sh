d3892b2553c3d2397083b7031a6c6841ec59d832
Rickard Doverfelt <rickard@doverfelt.se>
2015-07-22 23:33:23 +0200

    I'm just too tired, forgot to actually save the file...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,40 +1,32 @@
 #!/bin/bash
 
-/home/travis/build/solvillan/elektronswer/Elektronswer-linux-ia32
-/home/travis/build/solvillan/elektronswer/Elektronswer-win32-ia32
-/home/travis/build/solvillan/elektronswer/Elektronswer-darwin-x64
-/home/travis/build/solvillan/elektronswer/Elektronswer-linux-x64
-/home/travis/build/solvillan/elektronswer/Elektronswer-win32-x64
-
 #Remove bogus license
 rm /home/travis/build/solvillan/elektronswer/Elektronswer-*/LICENSE
 
 ## Linux
 echo "Packing linux"
 #64Bit
-tar -zcvf elektronswer-linux64.tar.gz Elektronswer-linux-x64
+tar -zcvf elektronswer-linux64.tar.gz /home/travis/build/solvillan/elektronswer/Elektronswer-linux-x64
 #32Bit
-tar -zcvf elektronswer-linux32.tar.gz ../Elektronswer-linux-ia32
+tar -zcvf elektronswer-linux32.tar.gz /home/travis/build/solvillan/elektronswer/Elektronswer-linux-ia32
 
 ## Windows
 echo "Packing windows"
 #64Bit
-zip -r elektronswer-win64.zip ../Elektronswer-win32-x64
+zip -r elektronswer-win64.zip /home/travis/build/solvillan/elektronswer/Elektronswer-win32-x64
 #32Bit
-zip -r elektronswer-win32.zip ../Elektronswer-win32-ia32
+zip -r elektronswer-win32.zip /home/travis/build/solvillan/elektronswer/Elektronswer-win32-ia32
 
 echo "Packing mac"
 ## Mac
 #64Bit
-zip -r elektronswer-mac64.zip ../Elektronswer-darwin-x64
+zip -r elektronswer-mac64.zip /home/travis/build/solvillan/elektronswer/Elektronswer-darwin-x64
 
 echo "Fixing permissions"
 chmod a+r elektronswer-*
 
 echo "Starting upload"
 ftp -n $FTP_SERVER <<End-Of-Session
-# -n option disables auto-logon
-
 user $FTP_USER "$FTP_PASS"
 binary
 cd /srv/http/
