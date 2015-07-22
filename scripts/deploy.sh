c5a2b0155dae2c24c787f4b835880df87fb228a1
Rickard Doverfelt <rickard@doverfelt.se>
2015-07-22 23:06:02 +0200

    Fixed deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,23 +4,28 @@
 rm Elektronswer-*/LICENSE
 
 ## Linux
+echo "Packing linux"
 #64Bit
 tar -zcvf elektronswer-linux64.tar.gz Elektronswer-linux-x64
 #32Bit
 tar -zcvf elektronswer-linux32.tar.gz Elektronswer-linux-ia32
 
 ## Windows
+echo "Packing windows"
 #64Bit
 zip -r elektronswer-win64.zip Elektronswer-win32-x64
 #32Bit
 zip -r elektronswer-win32.zip Elektronswer-win32-ia32
 
+echo "Packing mac"
 ## Mac
 #64Bit
 zip -r elektronswer-mac64.zip Elektronswer-darwin-x64
 
+echo "Fixing permissions"
 chmod a+r elektronswer-*
 
+echo "Starting upload"
 ftp -n $FTP_SERVER <<End-Of-Session
 # -n option disables auto-logon
 
@@ -34,3 +39,6 @@ put "elektronswer-win32.zip"
 put "elektronswer-mac64.zip"
 bye
 End-Of-Session
+
+echo "Done deploying!"
+return 0
