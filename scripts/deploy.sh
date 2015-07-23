1613c14bd1176169a38503b479ca2626bec18bba
Rickard Doverfelt <rickard@doverfelt.se>
2015-07-23 17:40:15 +0200

    Cleaned up build and fixed permissions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,30 +6,34 @@ rm *Elektronswer-*/LICENSE
 ## Linux
 echo "Packing linux"
 #64Bit
+rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-linux-x64/Elektronswer-*
 tar -zcvf elektronswer-linux64.tar.gz /home/travis/build/solvillan/elektronswer/Elektronswer-linux-x64
 #32Bit
+rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-linux-ia32/Elektronswer-*
 tar -zcvf elektronswer-linux32.tar.gz /home/travis/build/solvillan/elektronswer/Elektronswer-linux-ia32
 
 ## Windows
 echo "Packing windows"
 #64Bit
+rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-win32-x64/Elektronswer-*
 zip -r elektronswer-win64.zip /home/travis/build/solvillan/elektronswer/Elektronswer-win32-x64
 #32Bit
+rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-win32-ia32/Elektronswer-*
 zip -r elektronswer-win32.zip /home/travis/build/solvillan/elektronswer/Elektronswer-win32-ia32
 
 echo "Packing mac"
 ## Mac
 #64Bit
+rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-darwin-x64/Elektronswer-*
 zip -r elektronswer-mac64.zip /home/travis/build/solvillan/elektronswer/Elektronswer-darwin-x64
 
 echo "Fixing permissions"
-chmod a+r elektronswer-*
+chmod a+rw elektronswer-*
 
 echo "Starting upload"
 
 echo "open $FTP_SERVER
 user $FTP_USER \"$FTP_PASS\"
-binary
 cd /srv/http/
 put elektronswer-linux64.tar.gz
 put elektronswer-linux32.tar.gz
@@ -38,6 +42,6 @@ put elektronswer-win32.zip
 put elektronswer-mac64.zip
 bye" > upload
 
-lftp -f upload
+lftp -p -f upload
 
 echo "Done deploying!"
