d94612b313a7c911be2e5c5faff59ceb4cc50a4e
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2017-01-13 16:50:39 +0100

    Should fix deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,72 +7,29 @@ rm *Elektronswer-*/LICENSE
 echo "Packing linux"
 #64Bit
 rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-linux-x64/resources/app/Elektronswer-*
-tar -zcvf elektronswer-linux64-$TRAVIS_BUILD_NUMBER.tar.gz Elektronswer-linux-x64
+tar -zcvf elektronswer-linux64.tar.gz Elektronswer-linux-x64
 #32Bit
 rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-linux-x64/resources/app/Elektronswer-*
-tar -zcvf elektronswer-linux32-$TRAVIS_BUILD_NUMBER.tar.gz Elektronswer-linux-ia32
+tar -zcvf elektronswer-linux32.tar.gz Elektronswer-linux-ia32
 
 ## Windows
 echo "Packing windows"
 #64Bit
 rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-win32-x64/resources/app/Elektronswer-*
-zip -r elektronswer-win64-$TRAVIS_BUILD_NUMBER.zip Elektronswer-win32-x64
+zip -r elektronswer-win64.zip Elektronswer-win32-x64
 #32Bit
 rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-win32-ia32/resources/app/Elektronswer-*
-zip -r elektronswer-win32-$TRAVIS_BUILD_NUMBER.zip Elektronswer-win32-ia32
+zip -r elektronswer-win32.zip Elektronswer-win32-ia32
 
 echo "Packing mac"
 ## Mac
 #64Bit
 rm -rf /home/travis/build/solvillan/elektronswer/Elektronswer-darwin-x64/Elektronswer.app/Contents/Resources/app/Elektronswer-*
-zip -r elektronswer-mac64-$TRAVIS_BUILD_NUMBER.zip Elektronswer-darwin-x64
+zip -r elektronswer-mac64.zip Elektronswer-darwin-x64
 
 echo "Fixing permissions"
-chmod a+rw elektronswer-mac64-$TRAVIS_BUILD_NUMBER.zip
-chmod a+rw elektronswer-win32-$TRAVIS_BUILD_NUMBER.zip
-chmod a+rw elektronswer-win64-$TRAVIS_BUILD_NUMBER.zip
-chmod a+rw elektronswer-linux64-$TRAVIS_BUILD_NUMBER.tar.gz
-chmod a+rw elektronswer-linux32-$TRAVIS_BUILD_NUMBER.tar.gz
-
-echo "Generate webpage"
-
-echo "<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset=\"utf-8\">
-    <title>Elektronswer</title>
-  </head>
-  <body>
-    <h1>Welcome to Elektronswer.</h1>
-    <h6>Download latest builds:</h6><a class=\"buildnr\">Build #$TRAVIS_BUILD_NUMBER</a>
-    <ul>
-      <li><a href=\"elektronswer-linux64-$TRAVIS_BUILD_NUMBER.tar.gz\">Linux x64</a></li>
-      <li><a href=\"elektronswer-linux32-$TRAVIS_BUILD_NUMBER.tar.gz\">Linux x86</a></li>
-      <li><a href=\"elektronswer-win64-$TRAVIS_BUILD_NUMBER.zip\">Windows x64</a></li>
-      <li><a href=\"elektronswer-win32-$TRAVIS_BUILD_NUMBER.zip\">Windows x86</a></li>
-      <li><a href=\"elektronswer-mac64-$TRAVIS_BUILD_NUMBER.zip\">Mac OSX</a></li>
-    </ul>
-  </body>
-</html>
-" > index.html
-
-chmod a+rw index.html
-
-#chown http elektronswer-mac64-$TRAVIS_BUILD_NUMBER.zip elektronswer-win32-$TRAVIS_BUILD_NUMBER.zip elektronswer-win64-$TRAVIS_BUILD_NUMBER.zip elektronswer-linux64-$TRAVIS_BUILD_NUMBER.tar.gz elektronswer-linux32-$TRAVIS_BUILD_NUMBER.tar.gz index.html
-
-echo "Starting upload"
-
-echo "open $FTP_SERVER
-user $FTP_USER \"$FTP_PASS\"
-cd /srv/http/
-put elektronswer-linux64-$TRAVIS_BUILD_NUMBER.tar.gz
-put elektronswer-linux32-$TRAVIS_BUILD_NUMBER.tar.gz
-put elektronswer-win64-$TRAVIS_BUILD_NUMBER.zip
-put elektronswer-win32-$TRAVIS_BUILD_NUMBER.zip
-put elektronswer-mac64-$TRAVIS_BUILD_NUMBER.zip
-put index.html
-bye" > upload
-
-lftp -p -f upload
-
-echo "Done deploying!"
+chmod a+rw elektronswer-mac64.zip
+chmod a+rw elektronswer-win32.zip
+chmod a+rw elektronswer-win64.zip
+chmod a+rw elektronswer-linux64.tar.gz
+chmod a+rw elektronswer-linux32.tar.gz
